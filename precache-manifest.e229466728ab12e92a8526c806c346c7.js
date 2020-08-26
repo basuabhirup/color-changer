3fe93e15064e7a174041ff6799707161f2b601e5
basuabhirup <basu.abhirup@yahoo.com>
2020-08-26 16:36:19 +0530

    Updates

---
+++ precache-manifest.e229466728ab12e92a8526c806c346c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf765219000bd114edccbc9fc58b1e6",
+    "url": "/color-changer/index.html"
+  },
+  {
+    "revision": "831e84e7da78e9abe9f9",
+    "url": "/color-changer/static/css/main.df0cc3b5.chunk.css"
+  },
+  {
+    "revision": "417cf71eee0171dcc45b",
+    "url": "/color-changer/static/js/2.128a4432.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/color-changer/static/js/2.128a4432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831e84e7da78e9abe9f9",
+    "url": "/color-changer/static/js/main.79622640.chunk.js"
+  },
+  {
+    "revision": "fe5d89ecc88950b2eac8",
+    "url": "/color-changer/static/js/runtime-main.2bf0bacd.js"
+  }
+]);
